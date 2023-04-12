54168ffe365cba64e2d04cc647085c1618b9ba6b
Konul Yusifova <127186343+konulyusifova@users.noreply.github.com>
2023-04-12 13:50:15 +0400

    Add files via upload

---
+++ js/assets/js/main.js
@@ -0,0 +1,77 @@
+
+// var n=86;
+// a=n%10;
+// b=(n-a)/10
+// if(a>b){
+//     alert("a boyukdur");
+// }
+// else if(a<b){
+//     alert("b boyukdur");
+// }
+// else{
+//     alert("beraberdir");
+// }
+
+
+
+// var n=prompt("eded daxil edin:");
+// if(n>999 && n<10000){
+//     var a=n%10;
+//     var b=((n-a)/10)%10;
+//     var c=((((n-a)/10)-b)/10)%10;
+//     var d=(((((n-a)/10)-b)/10)-c)/10;
+//     var sum = a+b+c+d;
+//     alert(sum);
+// }
+// else{
+//     alert("Daxil etdiyiniz eded duzgun deyil!");
+// }
+
+
+
+// var month = prompt("Ay daxil edin");
+// switch (month) {
+//     case "yanvar":
+//         console.log("31");
+//         break;
+//     case "fevral":
+//         console.log("28 or 29");
+//         break;
+//     case "mart":
+//         console.log("31");
+//         break;
+//     case "aprel":
+//         console.log("30");
+//         break;
+//     case "may":
+//         console.log("31");
+//         break;
+//     case "iyun":
+//         console.log("30");
+//         break;
+//     case "iyul":
+//         console.log("31");
+//         break;
+//     case "avqust":
+//         console.log("31");
+//         break;
+//     case "sentyabr":
+//         console.log("30");
+//         break;
+//     case "oktyabr":
+//         console.log("31");
+//         break;
+//     case "noyabr":
+//         console.log("30");
+//         break;
+//     case "dekabr":
+//         console.log("31");
+//         break;
+// }
+
+
+
+
+// var num=prompt("eded daxil edin");
+
+// num%30==0 ? console.log("bolunur"): console.log("bolunmur");
